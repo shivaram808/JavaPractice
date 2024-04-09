41b8ac3714a361889af3183e5278441118ef490e
shivaram808 <shivaram.marella@gmail.com>
2024-04-09 12:37:55 +0530

    FAQ java programs practice again

---
+++ JavaPractice/src/Practiceprograms/FaqPracticeAgain.java
@@ -0,0 +1,28 @@
+package Practiceprograms;
+
+	import java.util.ArrayList;
+	import java.util.Collections;
+	import java.util.HashMap;
+
+	// import org.testng.annotations.Test;
+	
+	public class FaqPracticeAgain {
+		public static void main(String[] args) {
+
+			FaqPracticeAgain obj = new FaqPracticeAgain();
+			obj.testing();
+			
+		}
+
+
+		public void testing()
+		{
+			System.out.println("Just testing this method........");
+		}
+
+			}
+					
+					
+		
+		
+
